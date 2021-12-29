17c358164ea6433d7888fb8d22072d9840721322
Kacper <kacper2mail@gmail.com>
2021-12-29 13:21:29 +0100

    rss going to views

--- src/main/java/pl/kacperg/airlines/HomeController.java
+++ src/main/java/pl/kacperg/airlines/HomeController.java
@@ -1,13 +1,27 @@
 package pl.kacperg.airlines;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kacperg.airlines.rss.Feed;
+import pl.kacperg.airlines.rss.FeedMessage;
+import pl.kacperg.airlines.rss.RssFeedParser;
+
+import java.io.IOException;
+import java.net.URL;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @RequestMapping(value = "")
-    public String home(){
+    public String home(Model model) throws IOException {
+        RssFeedParser parser = new RssFeedParser("https://tvn24.pl/polska.xml");
+        Feed feed = parser.readFeed();
+        model.addAttribute("feed", feed);
         return "home/home";
     }
 }
